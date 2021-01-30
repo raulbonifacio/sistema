4bd51954d1f8df5b9356fe582f2ed3da51d71e1a
Raul Ribeiro Bonifácio <Raul Ribeiro Bonifcio>
2021-01-30 07:13:19 -0300

    - Transaction, models and the sequelize instance added to the globals.

--- src/app/business/index.js
+++ src/app/business/index.js
@@ -1,11 +1,15 @@
 const facade = require("./core/facade");
 const pipeline = require("./core/pipeline");
 
+const { transaction, sequelize, models } = require("../database/sequelize");
+
 const globals = {
+	transaction,
+	sequelize,
+	models,
 };
 
-const actions = {
-};
+const actions = {};
 
 const businessFacade = facade(actions, globals);
 
