4e700cdf839341ede96525d6fce276d7f82dc82a
Raul Ribeiro Bonifácio <Raul Ribeiro Bonifcio>
2021-01-30 18:21:31 -0300

    - Unnecessary methods removed.

--- src/app/business/core/output.js
+++ src/app/business/core/output.js
@@ -4,22 +4,6 @@ class Output {
 		this.errors = {};
 		this.warnings = {};
 	}
-
-	_has(target, ...props) {
-		//Check if has props
-	}
-
-	get hasData(...props) {
-		return props.length;
-	}
-
-	get hasErrors() {
-		return Object.keys(this.errors).length > 0;
-	}
-
-	get hasWarnings() {
-		return Object.keys(this.warnings).length > 0;
-	}
 }
 
 module.exports = Output;
